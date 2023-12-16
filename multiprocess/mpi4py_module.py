03e19ebd54d33f49ef9135e6081edcaa01553a2b
Uyen Vo <52061019+bichuyen99@users.noreply.github.com>
2023-12-16 17:08:03 +0300

    Update mpi4py_module.py

--- multiprocess/mpi4py_module.py
+++ multiprocess/mpi4py_module.py
@@ -15,6 +15,28 @@ def mpi_get_IOU(curr_masks, next_masks):
     Input: list of np.arrays
     Output: np.array (len(curr_masks) * len(next_masks))
     '''
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
     IOUs = np.zeros((len(curr_masks), len(next_masks)))
 
-    return IOUs
+    # Divide the work among processes
+    chunk_size = len(curr_masks) // size
+    start = rank * chunk_size
+    end = (rank + 1) * chunk_size if rank < size - 1 else len(curr_masks)
+
+    for i in range(start, end):
+        mask1 = (curr_masks[i] > 0).astype(int)
+        for j, mask2 in enumerate(next_masks):
+            mask2 = (mask2 > 0).astype(int)
+            iou = calculate_iou(mask1, mask2)
+            IOUs[i][j] = iou
+
+    # Gather results from all processes
+    all_IOUs = comm.gather(IOUs, root=0)
+
+    if rank == 0:
+        # Combine results from all processes
+        final_IOUs = np.vstack(all_IOUs)
+        return final_IOUs
